1cbc0ec3e8d5ed3ec27141efa1e9365d3cc91799
Obed <oa.dikki@gmail.com>
2023-06-20 00:55:12 +0200

    10th commit

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import IconArrowLeft from "../icon-arrow-left.svg";
 import InfoIcon from "../info-icon.svg";
-import { useSendbirdStateContext } from "@sendbird/uikit-react";
 import { ChannelSettings } from "./ChannelSettings";
 
 const ChatHeader = ({ channel, onBack }) => {
-  const globalStore = useSendbirdStateContext();
-  const user = globalStore?.stores?.userStore?.user;
   const [renderChannelSettings, setRenderChannelSettings] = useState(false);
 
   return (
